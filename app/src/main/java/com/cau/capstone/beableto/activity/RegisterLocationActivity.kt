1e8e1515caac54454b136741dedde8c55c2c8d97
SeongYunKim <cauboy17@naver.com>
2019-10-19 04:58:58 +0900

    SDK 최소 버전 수정 & Exif GPS값 위도, 경도로 변환

--- app/src/main/java/com/cau/capstone/beableto/activity/RegisterLocationActivity.kt
+++ app/src/main/java/com/cau/capstone/beableto/activity/RegisterLocationActivity.kt
@@ -4,18 +4,16 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.database.Cursor
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.FileUtils
+import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.net.toFile
+import androidx.exifinterface.media.ExifInterface
 import com.cau.capstone.beableto.R
 import com.cau.capstone.beableto.api.BEABLETOAPI
 import com.cau.capstone.beableto.api.NetworkCore
@@ -26,10 +24,8 @@ import kotlinx.android.synthetic.main.activity_register_location.*
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import retrofit2.http.Multipart
 import java.io.File
 import java.io.IOException
-import java.net.URI
 
 class RegisterLocationActivity : AppCompatActivity() {
 
@@ -136,11 +132,21 @@ class RegisterLocationActivity : AppCompatActivity() {
             val intent = Intent()
             intent.type = "image/*"
             intent.action = Intent.ACTION_GET_CONTENT
-            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_CODE)
-            }
-            else{
-                startActivityForResult(Intent.createChooser(intent, "SELECT PICTURE"), PICK_IMAGE_REQUEST)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSION_CODE
+                )
+            } else {
+                startActivityForResult(
+                    Intent.createChooser(intent, "SELECT PICTURE"),
+                    PICK_IMAGE_REQUEST
+                )
             }
 
             /*
@@ -164,26 +170,57 @@ class RegisterLocationActivity : AppCompatActivity() {
            */
         }
 
+        cb_use_photo.setOnClickListener {
+            var latitude : Float
+            var longtitude : Float
+            try {
+                val inputStream = getContentResolver().openInputStream(filePath!!)
+                val exif = ExifInterface(inputStream!!)
+                //Exif GPS 정보를 GeoPoint로 변환
+                val attrLatitude : String? = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE)
+                val attrLatitude_REF : String? = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF)
+                val attrLongtitute : String? = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE)
+                val attrLongtitute_REF : String? = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF)
+                if((attrLatitude != null) && (attrLatitude_REF != null) && (attrLongtitute != null) && (attrLongtitute_REF != null)){
+                    if(attrLatitude_REF == "N")
+                        latitude = convertToDegree(attrLatitude)
+                    else
+                        latitude = 0 - convertToDegree(attrLatitude)
+
+                    if(attrLongtitute_REF == "E")
+                        longtitude = convertToDegree(attrLongtitute)
+                    else
+                        longtitude = 0 - convertToDegree(attrLongtitute)
+
+                    Toast.makeText(this@RegisterLocationActivity, attrLatitude.toString(), Toast.LENGTH_SHORT).show()
+                    Log.d("GeoPoint", latitude.toString() + ", "+ longtitude.toString())
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+
         layout_register_location.setOnClickListener {
 
             val file = File(string_filePath)
             val fileReqBody = RequestBody.create(MediaType.parse("image/*"), file)
             val part = MultipartBody.Part.createFormData("image", file.name, fileReqBody)
 
-            var slope : Int = 0
+            var slope: Int = 0
             var auto_door: Boolean = true
             var elevator: Boolean = true
             var toilet: Boolean = true
 
-            if(layout_slope_none.isSelected) slope = 0;
-            else if(layout_slope_gentle.isSelected) slope = 1;
-            else if(layout_slope_sharp.isSelected) slope = 2;
-            if(layout_auto_door.isSelected) auto_door = true
-            else if(layout_hand_door.isSelected) auto_door = false
-            if(layout_elevator.isSelected) elevator = true
-            else if(layout_no_elevator.isSelected) elevator = false
-            if(layout_toilet.isSelected) toilet = true
-            else if(layout_no_toilet.isSelected) toilet = false;
+            if (layout_slope_none.isSelected) slope = 0;
+            else if (layout_slope_gentle.isSelected) slope = 1;
+            else if (layout_slope_sharp.isSelected) slope = 2;
+            if (layout_auto_door.isSelected) auto_door = true
+            else if (layout_hand_door.isSelected) auto_door = false
+            if (layout_elevator.isSelected) elevator = true
+            else if (layout_no_elevator.isSelected) elevator = false
+            if (layout_toilet.isSelected) toilet = true
+            else if (layout_no_toilet.isSelected) toilet = false;
             val x = 1.1F
             val y = 2.2F
 
@@ -203,7 +240,7 @@ class RegisterLocationActivity : AppCompatActivity() {
                 )
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({response ->
+                .subscribe({ response ->
                     Log.d("L_Register Success", response.message)
                     Toast.makeText(this@RegisterLocationActivity, "성공!!", Toast.LENGTH_SHORT).show()
                 }, {
@@ -214,25 +251,70 @@ class RegisterLocationActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null )
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null)
             filePath = data.data
-            string_filePath = getPathFromUri(data!!.data)
-                try{
-                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
-                iv_register_location!!.setImageBitmap(bitmap)
-            } catch (e : IOException) {
-                e.printStackTrace()
-            }
+        string_filePath = getPathFromUri(data!!.data)
+        try {
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
+            iv_register_location!!.setImageBitmap(bitmap)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 
-    fun getPathFromUri(uri: Uri?) : String {
-        val filePathColumn : Array<String> = arrayOf(MediaStore.Images.Media.DATA)
-        var cursor = getContentResolver().query(uri!!, filePathColumn, null, null, null )
-        startManagingCursor(cursor)
-        cursor!!.moveToFirst()
-        val columnIndex : Int = cursor.getColumnIndex(filePathColumn[0])
-        val path : String = cursor.getString(columnIndex)
-        cursor.close()
-        return path
+    //TODO SDK Version 27 체크
+    fun getPathFromUri(uri: Uri?): String {
+        val filePathColumn: Array<String> = arrayOf(MediaStore.Images.Media.DATA)
+
+        if (Build.VERSION.SDK_INT > 26) {
+            var path: String = ""
+            val fileId: String = DocumentsContract.getDocumentId(uri)
+            val id: String = fileId.split(":")[1]
+            val idString: Array<String> = arrayOf(id)
+            val selector: String = MediaStore.Images.Media._ID + "=?"
+            val cursor = getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                filePathColumn,
+                selector,
+                idString,
+                null
+            )
+            val columnIndex: Int = cursor!!.getColumnIndex(filePathColumn[0])
+            if (cursor.moveToFirst())
+                path = cursor.getString(columnIndex)
+            cursor.close()
+            return path
+        } else {
+            var cursor = getContentResolver().query(uri!!, filePathColumn, null, null, null)
+            startManagingCursor(cursor)
+            cursor!!.moveToFirst()
+            val columnIndex: Int = cursor.getColumnIndex(filePathColumn[0])
+            val path: String = cursor.getString(columnIndex)
+            cursor.close()
+            return path
+        }
+    }
+
+    fun convertToDegree(stringDMS : String) : Float{
+        var result : Float?
+        val DMS : List<String> = stringDMS.split(",")
+
+        val stringD : List<String> = DMS[0].split("/")
+        val d0 : Double = stringD[0].toDouble()
+        val d1 : Double = stringD[1].toDouble()
+        val floatD : Double = d0 / d1
+
+        val stringM : List<String> = DMS[1].split("/")
+        val m0 : Double = stringM[0].toDouble()
+        val m1 : Double = stringM[1].toDouble()
+        val floatM : Double = m0 / m1
+
+        val stringS : List<String> = DMS[2].split("/")
+        val s0 : Double = stringS[0].toDouble()
+        val s1 : Double = stringS[1].toDouble()
+        val floatS : Double = s0 / s1
+
+        result = (floatD + (floatM / 60) + (floatS / 3600)).toFloat()
+        return result
     }
 }
