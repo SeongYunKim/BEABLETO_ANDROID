acde74d55e2d368d04fbff0fa98ede28e96df37a
SeongYunKim <cauboy17@naver.com>
2019-11-27 04:58:32 +0900

    버스 정보 등록 기능 구현(Code written by @JunsukOH)

---
+++ app/src/main/java/com/cau/capstone/beableto/activity/RegisterBusActivity.kt
@@ -0,0 +1,13 @@
+package com.cau.capstone.beableto.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cau.capstone.beableto.R
+
+class RegisterBusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bus_register)
+    }
+}
